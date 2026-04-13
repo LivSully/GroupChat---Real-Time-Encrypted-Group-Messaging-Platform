f049f8b9aa7b44aceb3537267ef611e4d83409bc
Christopher R Marta <lujanc@merrimack.edu>
2026-04-13 16:23:03 -0400

    ADDED the datastructes to implement "rooms"

--- src/Server.java
+++ src/Server.java
@@ -6,11 +6,13 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Server {
     private static Server instance;
-
+    private Map<String, Room> rooms = new HashMap<>();
     public static Server getInstance() {
         if (instance == null)
             instance = new Server();
